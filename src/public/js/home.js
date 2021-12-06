853f9dae9fd7f1e4ddd6a16555e118a0dc588568
tymino <d_team-lex@bk.ru>
2021-12-06 19:20:04 +0300

    validate home page

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -10,11 +10,37 @@ const copy = document.querySelector('.js-link-page__copy');
 
 const MESSAGE_MAX_LENGTH = 500;
 
+const validateText = () => {
+  if (message.value.length < 1) {
+    message.classList.add('error');
+    return true;
+  }
+
+  message.classList.remove('error');
+  return false;
+};
+
+const validatePassword = () => {
+  if (password.value.length < 4 || password.value.length > 8) {
+    password.classList.add('error');
+    return true;
+  }
+
+  password.classList.remove('error');
+  return false;
+};
+
+const clearInputs = () => {
+  message.value = '';
+  password.value = '';
+}
+
 formBlock.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  if (message.value.length < 1) return;
-  if (password.value.length < 4 || password.value.length > 8) return;
+  const isValidText = validateText();
+  const isValidPassword = validatePassword();
+  if (isValidText || isValidPassword) return;
 
   const secretMessage = {
     message: message.value,
@@ -31,13 +57,24 @@ formBlock.addEventListener('submit', async (e) => {
   const json = await response.json();
 
   console.log(json);
+
+  clearInputs();
 });
 
 message.addEventListener('input', (e) => {
+  validateText();
   messageLimit.textContent = `${e.target.value.length}/${MESSAGE_MAX_LENGTH}`;
 
   if (e.target.value.length >= MESSAGE_MAX_LENGTH) {
     e.target.value = e.target.value.substring(0, MESSAGE_MAX_LENGTH);
     messageLimit.textContent = `${e.target.value.length}/${MESSAGE_MAX_LENGTH}`;
   }
+});
+
+password.addEventListener('input', (e) => {
+  if (e.target.value.length >= 8) {
+    e.target.value = e.target.value.substring(0, 8);
+    return;
+  }
+  validatePassword();
 });
